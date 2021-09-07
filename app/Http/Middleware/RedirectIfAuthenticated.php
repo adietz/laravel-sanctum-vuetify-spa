ee8770f386726756a95de74fc7774cc611e50224
Andy Dietz <adietz@cgshealth.com>
2021-09-07 08:49:06 -0500

    do not redirect if authenticated when request is from SPA

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,28 +2,30 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
 
-class RedirectIfAuthenticated
-{
+class RedirectIfAuthenticated {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  ...$guards
+     * @param Request $request
+     * @param Closure $next
+     * @param string|null ...$guards
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, ...$guards)
-    {
+    public function handle(Request $request, Closure $next, ...$guards) {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if (Auth::guard($guard)
+                    ->check()) {
+                if (!$request->expectsJson()) {
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
 
